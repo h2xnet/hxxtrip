6af771e3c168a01a1f2dc2ae2b96535f662a6add
xingguangzfs <fushou3579@163.com>
2023-01-13 08:16:18 +0800

    1

---
+++ components/AccountLogin/AccountLogin.vue
@@ -0,0 +1,65 @@
+<template>
+	<view class="container">
+		<uni-card :title="titleText">
+			<form @submit="formSubmit" @reset="formReset">
+				<view>
+					<view class="uni-forms-item margin-top-min">
+						<input id="userAccount" focus="true" type="text" :placeholder="accountLabelText"/>
+					</view>
+					
+					<view class="space-gray-line margin-top-min"></view>
+					
+					<view class="uni-forms-item margin-top-min">
+						<input id="userPswd" type="text" password="true" :placeholder="pswdLabelText"/>
+					</view>
+					
+				</view>
+				
+			</form>
+			
+		</uni-card>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"AccountLogin",
+		data() {
+			return {
+				titleText: '账号登录',
+				accountLabelText: '请输入账号',
+				pswdLabelText: '请输入密码'
+			};
+		},
+		methods: {
+			formSubmit: function(e) {
+				console.log('form发生了submit事件，携带数据为：' + JSON.stringify(e.detail.value))
+				var formdata = e.detail.value
+				uni.showModal({
+					content: '表单数据内容：' + JSON.stringify(formdata),
+					showCancel: false
+				});
+			},
+			formReset: function(e) {
+				console.log('清空数据')
+			}
+		}
+
+
+	}
+</script>
+
+<style>
+	
+	.margin-top-min {
+		margin-top: 15upx;
+	}
+	
+	// 灰色分隔线
+	.space-gray-line {
+		width: 90%;
+		height: 1upx;
+		background-color: #d9d9d9;
+	}
+
+</style>
--- pages/tabbar-5-detail/tabbar-5-detail-login/tabbar-5-detail-login.vue
+++ pages/tabbar-5-detail/tabbar-5-detail-login/tabbar-5-detail-login.vue
@@ -1,25 +1,52 @@
 <template>
-	<view>
+	<view class="container">
 		
-		<view class="phone-login-wrap">
-			
+		<view class="center-wrap">
+			<view class="center-container-wrap" style="width: 95%;">
+				<view v-if="loginMethod == 0">
+					<view class="line-box">
+						<view class="text-align-center">13560440305</view>
+					</view>
+					<view class="line-box" style="padding: 0;">
+						<button type="primary">手机号一键登录</button>
+					</view>
+				</view>	
+				<view v-else-if="loginMethod == 1">
+					<view class="line-box" style="padding: 0;">
+						<button type="primary">微信登录</button>
+					</view>
+				</view>
+				<view v-else-if="loginMethod == 2">
+					<view class="line-box">
+						QQ登录
+					</view>
+				</view>
+				<view v-else-if="loginMethod == 3">
+					<view class="line-box" style="padding: 0;">
+						<AccountLogin>
+							
+						</AccountLogin>
+					</view>
+				</view>
+				
+			</view>
 		</view>
 		
 		<view class="fixed-bottom" style="padding-bottom: 50upx;">
 			<view class="line-box">
-				<view class="line-box-item" style="margin-left: 30upx;">
+				<view class="line-box-item" style="margin-left: 30upx;" @click="onSetLoginMethod(0);">
 					<view class="font-size-max"><uni-icons type="phone" size="32" color="gray"></uni-icons></view>
 					<view class="font-size-max" style="padding-left: 8upx;">手机</view>
 				</view>
-				<view class="line-box-item" style="margin-left: 50upx;">
+				<view class="line-box-item" style="margin-left: 50upx;" @click="onSetLoginMethod(1);">
 					<view class="font-size-max"><uni-icons type="weixin" size="32" color="green"></uni-icons></view>
 					<view class="font-size-max" style="padding-left: 8upx;">微信</view>
 				</view>
-				<view class="line-box-item" style="margin-left: 50upx;">
+				<!--<view class="line-box-item" style="margin-left: 50upx;" @click="onSetLoginMethod(2);">
 					<view class="font-size-max"><uni-icons type="qq" size="32" color="skyblue"></uni-icons></view>
 					<view class="font-size-max" style="padding-left: 10upx;">QQ</view>
-				</view>
-				<view class="line-box-item" style="margin-left: 50upx;">
+				</view>-->
+				<view class="line-box-item" style="margin-left: 50upx;" @click="onSetLoginMethod(3);">
 					<view class="font-size-max"><uni-icons type="person" size="32" color="gray"></uni-icons></view>
 					<view class="font-size-max" style="padding-left: 8upx;">账号</view>
 				</view>
@@ -33,15 +60,29 @@
 	export default {
 		data() {
 			return {
-				
+				loginMethod: 0
 			}
 		},
 		methods: {
 			
+			onSetLoginMethod(method) {
+				console.log("tabbar-5-detail-login.vue onSetLoginMethod params, method:" + method);
+				let That = this
+				
+				That.loginMethod = method;
+			}
 		}
 	}
 </script>
 
 <style>
+	
+	.container {
+		width: 100%;
+		height: calc(100vh - 50upx);
+		background-color: white;
+	}
+	
+	
 
 </style>
